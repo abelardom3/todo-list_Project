d4446ee6ea4ee2351e207c77ea1793c8116d3fe6
abelardom3 <abelardom1999@gmail.com>
2022-09-05 15:53:10 -0400

    updated port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 require("dotenv").config();
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 const db = require('./dataBase/conn.js')
 
