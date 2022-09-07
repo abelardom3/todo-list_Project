2d86e2286628d070b5e9999dc98a2a6faf6267b2
abelardom3 <abelardom1999@gmail.com>
2022-09-06 21:50:39 -0400

    updates

--- dataBase/conn.js
+++ dataBase/conn.js
@@ -1,22 +1,22 @@
 const { Pool } = require('pg')
 
-// const pool = new Pool({
-//     user: "abela",
-//     password: "812315",
-//     host: "localhost",
-//     port: 5432,
-//     database: "todo_list_app"
-
-// })
-
-
-
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
+    user: "abela",
+    password: "812315",
+    host: "localhost",
+    port: 5432,
+    database: "todo_list_app"
+
+})
+
+
+
+// const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: {
+//         rejectUnauthorized: false
+//     }
+// });
 
 
 
